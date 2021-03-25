4d25ba36948b7de04cef51d19b5dc2fd8821a76c
carterfromparkway <81165375+carterfromparkway@users.noreply.github.com>
2021-03-25 09:31:05 -0400

    Add files via upload

---
+++ gaming1_1.ps1
@@ -0,0 +1,33 @@
+﻿#are you ready to play a game?
+
+$wsh = New-Object -ComObject Wscript.Shell
+#$wsh.Popup("description",<delay in seconds>,"popup title",<popup type, (ex: 3)>)
+
+
+carter: $wsh.Popup("Are you ready to Game??!?",4,"CVS Game Installer",3)
+carter: $wsh.Popup("too bad lmao",2,"CVS Game Installer",48)
+carter: $wsh.Popup("inspired by derk friday and braden left",2,"CVS Game Installer",16)
+carter: $wsh.Popup("you're computer now belongs to darth neil",2,"DARTH NEIL",16)
+
+
+neil1: $wsh.Popup("DARTH NEIL",2,"DARTH NEIL",64)
+neil2: $wsh.Popup("DARTH NEIL",2,"DARTH NEIL",64)
+neil3: $wsh.Popup("DARTH NEIL",2,"DARTH NEIL",64)
+
+
+#Start-Process "https://www.youtube.com/watch?v=lm5o50ICyMQ&t=0s"
+#Start-Process "https://www.youtube.com/watch?v=lm5o50ICyMQ&t=0s"
+#Start-Process "https://www.youtube.com/watch?v=lm5o50ICyMQ&t=0s"
+#Start-Process "https://www.youtube.com/watch?v=lm5o50ICyMQ&t=0s"
+
+
+#Start-Process "https://www.youtube.com/watch?v=5ToMX4iPI4w&t=0s"
+
+
+Start-Process "https://youtu.be/gQfFOPFLo0U?t=11893"
+while($true)
+{
+    $obj = new-object -com wscript.shell 
+    $obj.SendKeys([char]175)
+}
+
---
+++ readmepls.md
@@ -0,0 +1,3 @@
+Hello there!
+
+Version of of the "gaming" will spam someone's computer from the elusive and legendary Darth Neil.  This is still a work in progress and is just the first version
